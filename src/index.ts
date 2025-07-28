848d1aa0af55e6236337755da1173981d1ac680d
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-07-28 13:52:08 +0530

    feat!: mv from zod to StandardSchemaV1 and replace tsup with tsdown

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
-import * as uq from './functions';
+import qs from 'qs';
+import type { StandardSchemaV1 } from '@standard-schema/spec';
 
-export * from './functions';
-export default uq;
+export function encode<T>(data: T, options: qs.IStringifyOptions = { indices: true }): string {
+	return qs.stringify(data, options);
+}
+
+export async function parse<T extends StandardSchemaV1>(
+	params: URLSearchParams,
+	schema: T,
+	options: qs.IParseOptions = {}
+): Promise<StandardSchemaV1.InferOutput<T>> {
+	const result = qs.parse(params.toString(), options);
+
+	let parsedResult = schema['~standard'].validate(result);
+	if (parsedResult instanceof Promise) parsedResult = await parsedResult;
+
+	// if the `issues` field exists, the validation failed
+	if (parsedResult.issues) {
+		throw new Error(JSON.stringify(parsedResult.issues, null, 2));
+	}
+
+	return parsedResult.value;
+}
